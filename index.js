5445fdbae452700c2096b6a5e8d1035f8eaa5b36
Joshua <joshuawilliams7800@gmail.com>
2021-09-08 14:53:29 -0500

    working on the button

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const body = document.querySelector('body');
 const container = document.createElement('div');
 const btn = document.createElement('button');
-const title = document.querySelector('.title')
+const title = document.querySelector('.title');
 
 container.classList.add('container');
 body.append(container);
@@ -14,30 +14,35 @@ function findSides(sqrs) {
     return x;
 }
 
+function createSqr(x) {
+    const square = document.createElement('div');
+    square.className ='sqr';
+    square.setAttribute('style', `width: ${findSides(x)}px; height: ${findSides(x)}px`)
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'gray';
+    });
+    container.append(square);
+};
+
+
 function makeSqrs(x) {
     if (x > 64 || x < 4) {
         alert('Sorry, you must pick a number between 4 and 64...')
     } else {
         y = x * x;
         for(let i = 0; i < y; i++) {
-            const square = document.createElement('div');
-            square.className ='sqr';
-            square.setAttribute('style', `width: ${findSides(x)}px; height: ${findSides(x)}px`)
-            square.addEventListener('mouseenter', () => {
-                square.style.backgroundColor = 'gray';
-            });
-            container.append(square);
-        }
-        btn.addEventListener('click', () => {
-            // empty sketch old sqrs
-            let allSquares = document.querySelectorAll('.sqr');
-            container.removeChild('square')
-            // prompt user for number of sqrs per side, then input into makeSqrs()
-            makeSqrs(16)
-        })
-    }
+            createSqr(x)
+        };
+    };
 };
 
-
+btn.addEventListener('click', () => {
+    // empty sketch old sqrs
+    const allSqrs = document.querySelectorAll('.sqr');
+    allSqrs.remove();
+    // prompt user for number of sqrs per side, then input into makeSqrs()
+    const x = prompt('Pick the number of squares per side', placeholder=('must be between 4 - 64'));
+    makeSqrs(x);
+});
 
 makeSqrs(16);
